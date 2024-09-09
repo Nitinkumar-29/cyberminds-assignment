4fa638d02878b7e5d928d496440f64c635eec051
nitin <nimble2405@gmail.com>
2024-09-09 09:52:09 +0530

    added cors backend

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Fix for express.urlencoded
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.get("/", (req, res) => {
   // Fix for req, res
